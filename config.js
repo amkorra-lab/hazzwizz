6957e1ef17dc11c6f9a0531e58c706edf329dc42
amkorra-lab <juliane.neureither@gmail.com>
2025-12-25 14:51:50 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+const SKILLS = [
+  { key: "acrobatics", label: "Acrobatics", stat: "dex" },
+  { key: "arcana", label: "Arcana *", stat: "int" },
+  { key: "athletics", label: "Athletics", stat: "str" },
+  { key: "computers", label: "Computers **", stat: "int" },
+  { key: "crafting", label: "Crafting *", stat: "int" },
+  { key: "deception", label: "Deception", stat: "cha" },
+  { key: "diplomacy", label: "Diplomacy", stat: "cha" },
+  { key: "firstaid", label: "First Aid *", stat: "wis" },
+  { key: "insight", label: "Insight", stat: "wis" },
+  { key: "intimidation", label: "Intimidation", stat: "cha" },
+  { key: "investigation", label: "Investigation", stat: "wis" },
+  { key: "medicine", label: "Medicine **", stat: "int" },
+  { key: "nature", label: "Nature", stat: "wis" },
+  { key: "occultism", label: "Occultism", stat: "int" },
+  { key: "perception", label: "Perception", stat: "wis" },
+  { key: "performance", label: "Performance", stat: "cha" },
+  { key: "religion", label: "Religion", stat: "wis" },
+  { key: "slightofhand", label: "Slight of Hand", stat: "dex" },
+  { key: "society", label: "Society", stat: "int" },
+  { key: "stealth", label: "Stealth", stat: "dex" },
+  { key: "survival", label: "Survival", stat: "wis" },
+  { key: "techuse", label: "Tech Use *", stat: "int" },
+  { key: "vehicleuse", label: "Vehicle Use *", stat: "dex" },
+  // ...
+];
+
+const RACES = [
+  { key:"human"}
+];
